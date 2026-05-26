b7e0313ce5001348ff26384f66ddeb8792b583f6
goto <wanghaining5@hisilicon.com>
2026-05-26 18:45:14 +0800

    rename files

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export ASCEND_RT_VISIBLE_DEVICES=8
 export TRITON_END=mindspore
-pytest test_lightning_indexer_triton.py -v "$@"
-# python test_lightning_indexer_triton.py
+export TRITON_BACKEND=mindspore
+export TORCH_DEVICE_BACKEND_AUTOLOAD=0
+
+pytest test_li_triton.py -v "$@"
+# python test_li_triton.py
+# python test_sli_grad_kl_loss_triton.py
